b2b7b184d4b72b8ac8a51f5f4e7b971ac7195f62
Jazlyn <jonesjazlyn12@gmail.com>
2024-07-16 14:14:42 -0700

    Updated Header Import

--- peer-to-peer-ui/src/components/HomePage/HomePage.jsx
+++ peer-to-peer-ui/src/components/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import Header from "../Header/Header"
 import equipment from "/src/assets/equipment.png";
 import services from "/src/assets/services.png";
 import spaces from "/src/assets/spaces.png";
@@ -22,17 +23,7 @@ const HomePage = () => {
 					<img src={services} alt="Category 3" className="category-image" />
 				</Link>
 			</div>
-			{/* <div className='banner'>
-                <img src={banner} alt='Banner' className='banner-image' />
-                <div className='text'>
-                <div className='banner-text'>
-                    Break Into the Scene without Breaking the Bank
-                </div>
-                <div className='subtext'>
-                    Peer to Peer Photography Rental Services
-                </div>
-                </div>
-            </div> */}
+			
 			<div className="banner">
 				<div className="text">
 					<div className="banner-text">
