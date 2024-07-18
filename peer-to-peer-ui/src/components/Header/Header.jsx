6883b0e934ab8aa4efb52becca3a7ae70939c920
scbrown-224 <sydney.c.brown@vanderbilt.edu>
2024-07-18 14:37:34 -0700

    header w/ functioning equip search

--- peer-to-peer-ui/src/components/Header/Header.jsx
+++ peer-to-peer-ui/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
-
 import SearchBar from "../SearchBar/SearchBar";
 import "./Header.css";
 import logo from "/src/assets/logo.png";
@@ -8,33 +7,23 @@ import profileImg from "/src/assets/profile.png";
 import { Link } from "react-router-dom";
 
 const Header = ({ handleSubmit }) => {
-	return (
-		<div >
-			<h1 className="bigTitle">Peer2Peer</h1>
-	  <header className="header">
-		
-		<Link to="/home">
-		  <img src={logo} alt="Logo" className="logo" />
-		</Link>
-		<div className='headerBottom'>
-		  <NavBar />
-		  <SearchBar handleSubmit={handleSubmit} />
-
-		</div>
-		<Link to="/profile">
-			<img src={profileImg} style={{ height: "50px" }} />
-		</Link>
-		{/* <SearchBar handleSubmit={handleSubmit} /> */}
-
-	  </header>
-	  </div>
-	  
-
-	  
-	);
-  };
-
-
-
+    return (
+        <div>
+            <h1 className="bigTitle">Peer2Peer</h1>
+            <header className="header">
+                <Link to="/home">
+                    <img src={logo} alt="Logo" className="logo" />
+                </Link>
+                <div className='headerBottom'>
+                    <SearchBar handleSubmit={handleSubmit} />
+                    <NavBar />
+                </div>
+                <Link to="/profile">
+                    <img src={profileImg} style={{ height: "50px" }} />
+                </Link>
+            </header>
+        </div>
+    );
+};
 
 export default Header;
