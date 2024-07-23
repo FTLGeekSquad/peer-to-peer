68f4189decfd99853aedb6c35ac660c1a7bc674f
Jazlyn <jonesjazlyn12@gmail.com>
2024-07-23 15:26:59 -0700

    Merged from Main

--- peer-to-peer-ui/src/App.jsx
+++ peer-to-peer-ui/src/App.jsx
@@ -1,25 +1,72 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import NavBar from './components/NavBar/NavBar';
+// import SearchBar from './components/SearchBar/SearchBar';
+// import './App.css'
+// import HomePage from './components/HomePage/HomePage'
+// import Header from './components/Header/Header';
+// import ProfilePage from "./components/ProfilePage/ProfilePage";
+// import ServicesGrid from './components/ServicesPage/ServicesGrid';
+// import SpacesGrid from './components/SpacesPage/SpacesGrid';
+// import EquipmentGrid from './components/EquipmentPage/EquipmentGrid';
+
+// const Equipment = () => <div>Equipment Page</div>;
+// const Spaces = () => <div>Spaces Page</div>;
+// const Services = () => <div>Services Page</div>;
+// const Home = () => <div>Home Page</div>;
+
+// function App() {
+// 	return (
+// 	<Router>
+//     {/* <To be displayed on every page*/}
+
+// 		<Routes>
+// 			<Route path="/" element={<HomePage />} />
+// 			<Route path="/home" element={<HomePage />} />
+// 			<Route path="/equipment" element={<EquipmentGrid />} />
+// 			<Route path="/spaces" element={<SpacesGrid/>} />
+// 			<Route path="/services" element={<ServicesGrid/>} />
+// 			<Route path="/profile" element={<ProfilePage />} />
+// 		</Routes>
+// 	</Router>
+// 	);
+// }
+
+// export default App;
+
+// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes,Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import SearchBar from './components/SearchBar/SearchBar';
 import './App.css'
-import HomePage from './components/HomePage/HomePage';
+import HomePage from './components/HomePage/HomePage'
+import Header from './components/Header/Header';
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import ServicesGrid from './components/ServicesPage/ServicesGrid';
 import SpacesGrid from './components/SpacesPage/SpacesGrid';
 import EquipmentGrid from './components/EquipmentPage/EquipmentGrid';
+import Login from './components/GoogleOAuthStuff/Login';
+import Dashboard from './components/GoogleOAuthStuff/Dashboard';
+import PrivateRoute from './components/GoogleOAuthStuff/PrivateRoute';
+import Callback from './components/GoogleOAuthStuff/Callback';
 import { SavedListingsProvider } from './contexts/SavedListingsContext'; // Import the context provider
-import './App.css';
+
+
 
 function App() {
-  return (
-    <SavedListingsProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/equipment" element={<EquipmentGrid />} />
-          <Route path="/spaces" element={<SpacesGrid />} />
-          <Route path="/services" element={<ServicesGrid />} />
-          <Route path="/profile" element={<ProfilePage />} />
+	return (
+	<SavedListingsProvider>
+	<Router> 
+    {/* <To be displayed on every page*/}
+
+		<Routes>
+			<Route path="/" element={<HomePage />} /> 
+			<Route path="/home" element={<HomePage />} />
+			<Route path="/equipment" element={<EquipmentGrid />} />
+			<Route path="/spaces" element={<SpacesGrid/>} />
+			<Route path="/services" element={<ServicesGrid/>} />
+			<Route path="/profile" element={<ProfilePage />} />
 
 		
         	<Route path="/callback" element={<Callback />} />
@@ -32,12 +79,11 @@ function App() {
           }
         />
 		<Route path="/" element={<Navigate to="/login" />} />
-        </Routes>
-      </Router>
-    </SavedListingsProvider>
-  );
-}
-
+		</Routes>
+	</Router>
+	  </SavedListingsProvider>
+	);
+};
 export default App;
 
 
