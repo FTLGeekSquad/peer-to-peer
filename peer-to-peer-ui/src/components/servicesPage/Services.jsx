08d228e05d290eac7399a222f2bf56c234f162c8
scbrown-224 <sydney.c.brown@vanderbilt.edu>
2024-07-29 13:50:12 -0700

    prettier bookmark on category page

--- peer-to-peer-ui/src/components/servicesPage/Services.jsx
+++ peer-to-peer-ui/src/components/servicesPage/Services.jsx
@@ -1,13 +1,58 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import "./Services.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+
+// function Services({ onClick, listing, onSave }) {
+// 	// const { listingId, title, priceHourly, location } = listing;
+
+// 	const {
+// 		//instead of listing every param
+// 		listingId,
+// 		title,
+// 		description,
+// 		category,
+// 		subCategory,
+// 		priceHourly,
+// 		photo,
+// 		location,
+// 		//availability
+// 	} = listing;
+
+// 	return (
+// 		<div className="servicesCard">
+// 			<div onClick={() => onClick(listing)}>
+// 				{/* <img src={`https://picsum.photos/200?random=${listingId}`} alt={title} /> */}
+// 				<img src={photo} alt={title} />
+// 				<h3 className="servicesCardTitle">{title}</h3>
+// 				<div className="paragraph">
+// 					<p className="price">${priceHourly} per hour</p>
+// 					<p className="location">{location}</p>
+// 				</div>
+// 			</div>
+// 			<button className="bookmark-button" onClick={() => onSave(listing)}>
+// 				<FontAwesomeIcon icon={faBookmark} />
+// 			</button>
+// 		</div>
+// 	);
+// }
+
+// export default Services;
+
+import React, { useState } from "react";
 import "./Services.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 function Services({ onClick, listing, onSave }) {
-	// const { listingId, title, priceHourly, location } = listing;
+	const [isSaved, setIsSaved] = useState(false); // State to track if the item is saved
+
+	const handleSave = () => {
+		setIsSaved(!isSaved); // Toggle the saved state
+		onSave(listing); // Call the onSave function passed as a prop
+	};
 
 	const {
-		//instead of listing every param
 		listingId,
 		title,
 		description,
@@ -16,13 +61,11 @@ function Services({ onClick, listing, onSave }) {
 		priceHourly,
 		photo,
 		location,
-		//availability
 	} = listing;
 
 	return (
 		<div className="servicesCard">
 			<div onClick={() => onClick(listing)}>
-				{/* <img src={`https://picsum.photos/200?random=${listingId}`} alt={title} /> */}
 				<img src={photo} alt={title} />
 				<h3 className="servicesCardTitle">{title}</h3>
 				<div className="paragraph">
@@ -30,7 +73,10 @@ function Services({ onClick, listing, onSave }) {
 					<p className="location">{location}</p>
 				</div>
 			</div>
-			<button className="bookmark-button" onClick={() => onSave(listing)}>
+			<button
+				className={`bookmark-button ${isSaved ? "active" : ""}`}
+				onClick={handleSave}
+			>
 				<FontAwesomeIcon icon={faBookmark} />
 			</button>
 		</div>
