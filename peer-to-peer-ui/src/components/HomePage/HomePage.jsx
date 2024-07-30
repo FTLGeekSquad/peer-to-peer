66d54837f568d9a5c4253ee537c72ad29256f87f
RoxanaCruz <rox21cruz@gmail.com>
2024-07-30 16:23:18 -0700

    footer css

--- peer-to-peer-ui/src/components/HomePage/HomePage.jsx
+++ peer-to-peer-ui/src/components/HomePage/HomePage.jsx
@@ -59,14 +59,17 @@ const HomePage = () => {
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<LinkedInIcon fontSize="large" />
+										<LinkedInIcon
+											fontSize="large"
+											style={{ color: "#0e76a8" }}
+										/>
 									</a>
 									<a
 										href="https://github.com/RoxanaCruz"
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<GitHubIcon fontSize="large" />
+										<GitHubIcon fontSize="large" style={{ color: "#333" }} />
 									</a>
 								</div>
 								<p className="description">
@@ -82,14 +85,17 @@ const HomePage = () => {
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<LinkedInIcon fontSize="large" />
+										<LinkedInIcon
+											fontSize="large"
+											style={{ color: "#0e76a8" }}
+										/>
 									</a>
 									<a
 										href="https://github.com/scbrown-224"
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<GitHubIcon fontSize="large" />
+										<GitHubIcon fontSize="large" style={{ color: "#333" }} />
 									</a>
 								</div>
 								<p className="description">
@@ -107,14 +113,17 @@ const HomePage = () => {
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<LinkedInIcon fontSize="large" />
+										<LinkedInIcon
+											fontSize="large"
+											style={{ color: "#0e76a8" }}
+										/>
 									</a>
 									<a
 										href="https://github.com/JazlynJ1212"
 										target="_blank"
 										rel="noopener noreferrer"
 									>
-										<GitHubIcon fontSize="large" />
+										<GitHubIcon fontSize="large" style={{ color: "#333" }} />
 									</a>
 								</div>
 								<p className="description">
@@ -139,46 +148,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import "./HomePage.css";
-// import Header from "../Header/Header";
-// import equipment from "/src/assets/equipment.png";
-// import services from "/src/assets/services.png";
-// import spaces from "/src/assets/spaces.png";
-// import FileUpload from "../FileUpload/FileUpload";
-
-// const HomePage = () => {
-// 	return (
-// 		<div className="home-page">
-// 			<div>
-// 				<Header />
-// 			</div>
-// 			<div className="categories">
-// 				<Link to="/equipment" className="category-link">
-// 					<img src={equipment} alt="Category 1" className="category-image" />
-// 				</Link>
-// 				<Link to="/spaces" className="category-link">
-// 					<img src={spaces} alt="Category 2" className="category-image" />
-// 				</Link>
-// 				<Link to="/services" className="category-link">
-// 					<img src={services} alt="Category 3" className="category-image" />
-// 				</Link>
-// 			</div>
-
-// 			<div className="banner">
-// 				<div className="text">
-// 					<div className="banner-text">
-// 						Break Into the Scene without Breaking the Bank
-// 					</div>
-// 					<div className="subtext">
-// 						Peer to Peer Photography Rental Services
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default HomePage;
