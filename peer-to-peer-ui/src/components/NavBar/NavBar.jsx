71c6d9fabf2d9a5d7d6d8aff181c12f8edd560d6
scbrown-224 <sydney.c.brown@vanderbilt.edu>
2024-08-05 11:52:57 -0700

    removing some ugly comments

--- peer-to-peer-ui/src/components/NavBar/NavBar.jsx
+++ peer-to-peer-ui/src/components/NavBar/NavBar.jsx
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './NavBar.css';
-
-// const NavBar = () => {
-//   return (
-//     <nav className="navbar">
-//       <ul className="nav-links">
-//       <li>
-//           <Link to="/home">Home</Link>
-//         </li>
-//         <li>
-//           <Link to="/equipment">Equipment</Link>
-//         </li>
-//         <li>
-//           <Link to="/spaces">Spaces</Link>
-//         </li>
-//         <li>
-//           <Link to="/services">Services</Link>
-//         </li>
-        
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
-
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
