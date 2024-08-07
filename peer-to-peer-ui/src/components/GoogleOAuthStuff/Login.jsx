27ef2d4d430ceaec593b5cd425558f966b71abf3
RoxanaCruz <rox21cruz@gmail.com>
2024-08-07 11:35:13 -0700

    fixing login link

--- peer-to-peer-ui/src/components/GoogleOAuthStuff/Login.jsx
+++ peer-to-peer-ui/src/components/GoogleOAuthStuff/Login.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Login = () => {
+	const handleLogin = () => {
+		window.location.href = "http://peer-to-peer-59rz.onrender.com/auth/login";
+	};
 
-  const handleLogin = () => {
-    window.location.href = "https://peer-to-peer-59rz.onrender.com/auth/login";
-  };
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <button onClick={handleLogin}>Log in with Google</button>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Login</h2>
+			<button onClick={handleLogin}>Log in with Google</button>
+		</div>
+	);
 };
 
 export default Login;
